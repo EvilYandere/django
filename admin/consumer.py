ceca36970ce975ad0b939f736817ebe6b54ddd4c
UselessAqua <UselessAquaticKat@gmail.com>
2023-12-11 17:23:02 +0400

    Часть проекта на Django

---
+++ admin/consumer.py
@@ -0,0 +1,34 @@
+import json, pika, os, django
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin.settings')
+django.setup()
+
+
+from products.models import Product
+
+
+params = pika.URLParameters('amqps://zgjdnemk:mazfHa2ilWv0vBaI6g3jeDRQODDeLtTA@fish.rmq.cloudamqp.com/zgjdnemk')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+
+def callback(ch, method, properties, body):
+    print('Получил сообщение')
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes += 1
+    product.save()
+    print('Количество лайков увеличено')
+
+
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
+
+channel.close()
